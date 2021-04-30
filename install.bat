5be62ba8febbf010c50f333f87c836960a22f7dc
jhonfer <jhonfer.ruiz@gmail.com>
2021-04-30 16:09:12 -0500

    validated functions

--- install.bat
+++ install.bat
@@ -1,5 +1,16 @@
 @echo off
-mkdir ~\AppData\Local\nvim
-move config* ~\AppData\Local\nvim
-move init.vim ~\AppData\Local\nvim
-move .vimrc %USERPROFILE%
+
+REM Fija la ruta de la carpeta donde se guarda los datos
+set pathDestination=%USERPROFILE%\AppData\Local\nvim\
+
+REM Comprueba la existencia de la carpeta antes de crearla
+if not exist %pathDestination% (mkdir %pathDestination%)
+
+REM Comprueba la existencia de los archivos config antes de ejecutar el comando
+if exist config* (move config* %pathDestination%)
+
+REM Comprueba la existencia del archivo init.vim antes de ejecutar el comando
+if exist init.vim (move init.vim %pathDestination%)
+
+REM Comprueba la existencia del archivo .vimrc antes de ejecutar el comando
+if exist .vimrc (move .vimrc %USERPROFILE%)
